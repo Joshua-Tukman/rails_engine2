acd82dd84220d80626bcb1032bbc03c09688afb4
Josh Tukman <josh.tukman@gmail.com>
2020-07-14 13:59:00 -0600

    Passing tests for all crud functionality dealing with items resource

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -36,4 +36,35 @@ RSpec.describe "Items API" do
     item = json[:data]
     expect(item[:attributes][:name]).to eq("Cold Beer")
   end
+
+  it "can delete an item" do
+    merchant = create(:merchant)
+    item_params = { name: "Cold Beer", description: "Super tasty and refreshing", unit_price: 10, merchant_id: merchant.id}
+
+    post "/api/v1/items", params: item_params 
+
+    item = Item.last
+
+    expect(item.name).to eq("Cold Beer")
+    
+    delete "/api/v1/items/#{item.id}"
+    
+    expect(response).to be_successful
+    expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it "can update an item" do
+    merchant = create(:merchant)
+    id = create(:item, merchant_id: merchant.id).id
+    
+    previous_name = Item.last.name
+    item_params = { name: "Skull and Sword" }
+
+    put "/api/v1/items/#{id}", params: item_params
+    item = Item.find_by(id: id)
+    expect(response).to be_successful
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+    expect(json_response[:data][:attributes][:name]).to eq(item.name)
+  end
 end
