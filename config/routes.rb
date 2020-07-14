de7ba3e880f8a0aea13347d1a54670ce81f0e6e1
Josh Tukman <josh.tukman@gmail.com>
2020-07-13 18:21:31 -0600

    Add namespaced routes for api/v1/items_controller with index, show and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get '/customers', to: 'customers#index'
   
+  get '/customers', to: 'customers#index'
+  namespace :api do
+    namespace :v1 do
+      resources :items, only: [ :index, :show, :create ]
+    end
+  end
 end
