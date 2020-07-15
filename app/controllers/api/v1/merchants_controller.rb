ab007ed1a57d6ef28ba6422eae054228550a804c
Josh Tukman <josh.tukman@gmail.com>
2020-07-14 19:26:51 -0600

    Add all CRUD actions to merchant controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,7 +1,34 @@
 class Api::V1::MerchantsController < ApplicationController
+  def index
+    merchants = Merchant.all 
+    render json: MerchantSerializer.new(merchants)
+  end
 
   def show
-    merchant = Merchant.find(params["id"])
+    merchant = Merchant.find(params[:id])
     render json: MerchantSerializer.new(merchant)
+  end
+
+  def create
+    merchant = Merchant.create(merchant_params)
+    render json: MerchantSerializer.new(merchant)
+  end
+
+  def destroy
+    merchant = Merchant.find(params[:id])
+    merchant.delete
+    render json: MerchantSerializer.new(merchant)
+  end
+
+  def update
+     merchant = Merchant.find(params[:id])
+     merchant.update(merchant_params)
+     render json: MerchantSerializer.new(merchant)
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:name)
   end
 end
